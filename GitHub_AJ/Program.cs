544257aa72753114c8c9960db35c6ae9ee18479b
AleixJuR <154283119+AleixJuR@users.noreply.github.com>
2024-01-31 20:28:27 +0100

    Update Program.cs

--- GitHub_AJ/Program.cs
+++ GitHub_AJ/Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Ajr");
+            Console.WriteLine("Hello, Montilivi!");
         }
     }
 }
